85c49181dd618eb43faeec60501cb5c4cac914c9
james <wallparse@gmail.com>
2020-09-08 17:06:02 +0200

    Added first file

---
+++ checkapps.ps1
@@ -0,0 +1,101 @@
+﻿# James - 2020, 
+# This script just output some permission-sets of the Azure apps.
+
+param ([string] $file="output.csv")
+
+$delimiter = "|"
+$delimiter2 = ","
+
+
+# Check Azure connection (TODO: error handle this)
+try 
+{
+    $strDetails = Get-AzureADTenantDetail
+} 
+catch 
+{
+    write-host "[-] Please connect to Azure AD first..."
+    Connect-AzureAD
+
+    try 
+    {
+        $strDetails = Get-AzureADTenantDetail
+    } 
+    catch 
+    {
+        write-host "[-] Error... hmmm... guess something else is wrong..."
+        return
+    }
+}
+
+
+# Open file for output
+$swOut = new-object System.IO.StreamWriter( $file)
+
+# Fetch apps
+$allApps = get-azureadapplication -All $true
+
+# Write columns
+$columns = "DisplayName", "ObjectId", "AvailableToOtherTenants","SignInAudience","PublicClient","IsDisabled","AllowGuestsSignIn","ObjectType","IdentifierUris","AppRoles", "Oauth2Permissions","AssignedAppRoles"
+
+foreach($strColumn in $columns)
+{
+    $swOut.Write("$strColumn$delimiter")
+}
+
+$swOut.WriteLine()
+
+# Go through all apps
+foreach($app in $allApps)
+{
+    $strInfo = $app.DisplayName + $delimiter + $app.ObjectId + $delimiter + $app.AvailableToOtherTenants + $delimiter + $app.SignInAudience + $delimiter + $app.PublicClient + $delimiter + ` 
+    $app.IsDisabled + $delimiter + $app.AllowGuestsSignIn + $delimiter + $app.ObjectType + $delimiter + [System.String]::Join(",",$app.IdentifierUris.ToArray()) `
+    + $delimiter 
+
+    $strAppRoles = "";
+
+    foreach($appRole in $app.AppRoles)
+    {
+        $strAppRoles += $appRole.DisplayName + $delimiter2 + $appRole.IsEnabled + $delimiter2 + $appRole.Value
+    }
+
+    $strOauth2Permissions = "";
+
+    foreach($oauthPerm in $app.Oauth2Permissions)
+    {
+        $strOauth2Permissions += "[" + $oauthPerm.Type + $delimiter2 + $oauthPerm.UserConsentDescription + $delimiter2 + $oauthPerm.AdminConsentDescription + "]"
+    }
+
+    $strInfo += $strAppRoles
+    $strInfo += $delimiter
+    $strInfo += $strOauth2Permissions
+
+    $strAppAssignments = ""
+    $strDisplay = "displayName eq '" + $app.DisplayName +"'"
+    $appServicePrinciple = Get-AzureADServicePrincipal -All $true -Filter $strDisplay
+
+    foreach($strObjectId in $appServicePrinciple.ObjectId)
+    {
+
+        $appAssignments = Get-AzureADServiceAppRoleAssignedTo -All $true -ObjectId $strObjectId
+
+        if($appAssignments -ne $null)
+        {
+            foreach($appAssignment in $appAssignments)
+            {
+                $strAppAssignments += "["+ $appAssignment.ResourceDisplayName + ":" + $appAssignment.CreationTimeStamp + "]" + ","
+            }
+
+            #write-host $strAppAssignments
+        }
+    }
+
+    $strFinished =  "$strInfo$delimiter$strAppAssignments"
+
+    write-host $strFinished
+
+    $swOut.WriteLine($strFinished);
+}
+
+# Cleanup
+$swOut.Close()
